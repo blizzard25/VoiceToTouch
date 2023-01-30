7641d11bf2712ae1224f8364026ed5420fad3489
Blizzard25 <esanders@protonmail.com>
2023-01-30 12:11:35 -0600

    Add extremely basic UI and skeleton methods in MainActivity to start the input touch service and then hide the UI on touch

--- app/src/main/java/com/example/voicetotouch/MainActivity.java
+++ app/src/main/java/com/example/voicetotouch/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.voicetotouch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startServiceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startServiceButton = findViewById(R.id.start_service_button);
+        startServiceButton.setOnClickListener(v -> {
+            startTouchInputService();
+            hideUserInterface();
+        });
+    }
+
+    private void startTouchInputService() {
+        //Intent intent = new Intent(this, TouchInputService.class);
+        //startService(intent);
+    }
+
+    private void hideUserInterface() {
+        startServiceButton.setVisibility(View.GONE);
     }
 }
